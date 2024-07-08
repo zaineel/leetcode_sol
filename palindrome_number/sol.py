58977063c2d654d74239589502f12d6c284449f1
zaineel <127276018+zaineel@users.noreply.github.com>
2024-07-07 23:47:48 -0500

    added solution

---
+++ palindrome_number/sol.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        return (x == x[::-1])
+        
