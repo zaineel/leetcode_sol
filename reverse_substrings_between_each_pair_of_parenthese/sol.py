3757640ea3a62b2d85b5577e658d65e6f15e4b35
zaineel <127276018+zaineel@users.noreply.github.com>
2024-07-11 01:32:25 -0500

    solution added

---
+++ reverse_substrings_between_each_pair_of_parenthese/sol.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if char == ')':
+                substr = []
+                while stack and stack[-1] != '(':
+                    substr.append(stack.pop())
+                if stack:
+                    stack.pop()
+                stack.extend(substr)
+            else:
+                stack.append(char)
+        return ''.join(stack)
+
+        
