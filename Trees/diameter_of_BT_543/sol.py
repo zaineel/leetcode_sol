d4efe284db992a503968bcd167b4991a22ddb042
zaineel <127276018+zaineel@users.noreply.github.com>
2025-01-03 00:07:10 -0600

    added solution

---
+++ Trees/diameter_of_BT_543/sol.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        largest_diameter = [0]
+
+        def height(root):
+            if root is None:
+                return 0
+
+            left_height = height(root.left)
+            right_height = height(root.right)
+            diameter = left_height + right_height
+
+            largest_diameter[0] = max(largest_diameter[0], diameter)
+
+            return (1 + max(left_height, right_height))
+
+        height(root)
+        return largest_diameter[0]
+        
