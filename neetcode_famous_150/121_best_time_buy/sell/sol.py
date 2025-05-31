51b65cdeb92e3c66f0d22734545640cd69820eb8
zaineel <127276018+zaineel@users.noreply.github.com>
2025-05-31 18:47:38 -0500

    121 done

---
+++ neetcode_famous_150/121_best_time_buy/sell/sol.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                maxP = max(maxP, prices[r]-prices[l])
+            else:
+                l = r
+            r += 1
+        return maxP
