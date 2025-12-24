7c443feb9601afc63e21a8910312240b98379d0f
zaineel <127276018+zaineel@users.noreply.github.com>
2025-12-24 13:54:01 -0600

    sharpening

---
+++ GS/search_in_grid_II/sol.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInGrid(self, matrix: list[list[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        row = 0
+        col = cols - 1
+
+        while row < rows and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                col -= 1
+            else:
+                row += 1
+        return False
