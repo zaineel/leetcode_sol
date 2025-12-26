4583a26f322e6fdd4ae522150fd109eebddc8998
zaineel <127276018+zaineel@users.noreply.github.com>
2025-12-26 16:42:06 -0600

    sharpening

---
+++ GS/trapping_rain_water/sol.py
@@ -0,0 +1,18 @@
+class Solution:
+    def trappingRainWater(self, height: list[int]) -> int:
+        if not height:
+            return 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        water = 0
+
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
+        return water
