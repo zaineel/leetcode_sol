e04156cd14b3d10bc8aa2f0d0746fcf1910ac5a0
zaineel <127276018+zaineel@users.noreply.github.com>
2026-01-09 16:56:34 -0600

    134

---
+++ neetcode_famous_150/134_gas_station/sol.py
@@ -0,0 +1,12 @@
+class Solution:
+    def gasStation(self, gas: list[int], cost: list[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        total = 0
+        start = 0
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+            if total < 0:
+                total = 0
+                start = i + 1
+        return start
