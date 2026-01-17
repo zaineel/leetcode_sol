d68d58f87647a7793d5e060ae72e67e05395a1e8
zaineel <127276018+zaineel@users.noreply.github.com>
2026-01-17 15:47:13 -0600

    371

--- GS/climbing_stairs/sol.py
+++ GS/climbing_stairs/sol.py
@@ -6,3 +6,10 @@ class Solution:
             one = one + two
             two = temp
         return one
+    
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
